f46f99030a48866aafefe9515cdfcaaf79365732
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-11-06 19:11:20 +0100

    api log

--- DjangoLog/settings.py
+++ DjangoLog/settings.py
@@ -115,11 +115,14 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets')
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'DjangoLog/assets')
+]
+
 
 MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
